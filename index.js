e14b780b74d2abbd39aada50c5c4eea23b910034
groovecoder <luke.crouch@gmail.com>
2015-06-06 17:31:37 -0500

    read guage via promise and output result

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var cheerio = require('cheerio');
 var express = require('express');
+var Promise = require('promise');
 var req = require('request');
 var app = express();
 
@@ -9,12 +10,18 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   // 1. http://waterdata.usgs.gov/usa/nwis/uv?07164500
   // #WaterInTheRiver means more than 1' at the gauge, with 2' being a nice looking river.
-  req('http://waterdata.usgs.gov/usa/nwis/uv?07164500', function(error, response, body) {
-    console.log(body);
-    $ = cheerio.load(body);
-    console.log($('div.stationContainer').text());
+  var guageHeight = null;
+  var promise = new Promise(function(resolve, reject) {
+    req('http://waterdata.usgs.gov/usa/nwis/uv?07164500', function(error, resp, body) {
+      if (error) reject(error);
+      else {
+        var valueRE = /value: (\d+\.\d+)/;
+        guageHeight = parseFloat(valueRE.exec(body)[1]);
+        console.log("guageHeight: " + guageHeight);
+        resolve(guageHeight);
+      }
+    });
   });
-    
   //
   //
   // 2. Information from Keystone (http://www.swt-wc.usace.army.mil/webdata/gagedata/KEYO2.current.html)
@@ -29,7 +36,16 @@ app.get('/', function(request, response) {
   // The values can change, at some point a person died below grand lake dam, which resulted in them turning it off while they looked for them. To make up for that, Keystone turned on. The charts aren't updated.
   // Again, ~6 hours after they say they will generate, riverside will start seeing water. 
 
-  response.send('Hello World!');
+  promise.then(function() {
+    var output = '';
+    if (guageHeight > 2) {
+      output += "Yes. ";
+    } else {
+      output += "No. ";
+    }
+    output += "Guage Height: " + guageHeight;
+    response.send(output);
+  });
 });
 
 app.listen(app.get('port'), function() {
